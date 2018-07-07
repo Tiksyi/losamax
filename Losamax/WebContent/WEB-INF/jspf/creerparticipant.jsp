021c84c3ef6a8d40cc99b1d5deff693fc60503cf
Mathieu <Mathieu@Camille-Mathieu>
2018-07-07 23:57:17 +0200

    Ni Dieux Ni Maitres

---
+++ Losamax/WebContent/WEB-INF/jspf/creerparticipant.jsp
@@ -0,0 +1,57 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ taglib uri="http://www.springframework.org/tags/form" prefix="form"%>
+<%@ taglib uri="http://www.springframework.org/tags" prefix="spring"%>
+<%@ taglib uri="http://www.springframework.org/security/tags"
+	prefix="sec"%>
+<%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions"%>
+<%@ page language="java" contentType="text/html; charset=utf-8"
+	pageEncoding="utf-8"%>
+<!DOCTYPE html>
+<html lang=fr>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title><spring:message code="creer.menu.title" /></title>
+<link rel="stylesheet"
+	href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script
+	src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script
+	src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<div class="container">
+		<h3 text-align=center>
+			<spring:message code="creer.menu.title" />
+		</h3>
+		<form method="POST" action="creerParticipant"
+			modelAttribute="participant">
+			<input type="hidden" name="${_csrf.parameterName}"
+				value="${_csrf.token}" /> <input type="hidden"
+				name="participant.id" />
+			<div class="form-group">
+				<form:label path="participant.nom">
+					<spring:message code="participant.nom" />
+				</form:label>
+				<form:input path="participant.nom" class="form-control" />
+			</div>
+			<div class="form-group">
+				<form:label path="participant.sport.id">
+					<spring:message code="participant.sport" />
+				</form:label>
+				<form:select path="participant.sport.id" class="form-control">
+					<form:option value="" label="" />
+					<form:options items="${sports}" itemValue="id" itemLabel="nom" />
+				</form:select>
+			</div>
+			<br> <input type="submit" class="btn btn-primary"
+				value="<spring:message code="participant.creer" />" />
+		</form>
+
+
+
+		<br> <a href="<c:url value="/paricontroller/goToMenu" />"
+			type="button" class="btn btn-primary"><spring:message
+				code="menu.retour" /></a>
+	</div>
+</body>
+</html>
--- Losamax/src/com/losamax/controllers/AdminController.java
+++ Losamax/src/com/losamax/controllers/AdminController.java
@@ -1,11 +1,17 @@
 package com.losamax.controllers;
 
+import java.util.Date;
 import java.util.List;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.losamax.dao.IEvenementJpaRepository;
@@ -14,6 +20,7 @@ import com.losamax.dao.IParticipantJpaRepository;
 import com.losamax.dao.ISportJpaRepository;
 import com.losamax.entities.Client;
 import com.losamax.entities.Evenement;
+import com.losamax.entities.Participant;
 import com.losamax.entities.Sport;
 
 @Controller
@@ -22,21 +29,40 @@ public class AdminController {
 
 	@Autowired
 	private ISportJpaRepository sportRepo;
-	
+
 	@Autowired
 	private IEvenementJpaRepository evenementRepo;
-	
+
 	@Autowired
 	private IParticipantJpaRepository participantRepo;
-	
+
 	@Autowired
 	private IPariJpaRepository pariRepo;
-	
 
 	@GetMapping("/goToWelcomeAdmin")
 	public String goToCreer(Model model) {
-//		List<Evenement> evenements = evenementRepo.findAll();
-		model.addAttribute("evenement", new Evenement());
+		List<Evenement> evenements = evenementRepo.findAll();
+		for (Evenement e : evenements)
+		{
+			e.setDateDebut(new Date());
+			e.setDateFin(new Date());
+		}
+		model.addAttribute("evenements", evenements);
 		return "bonjouradmin";
 	}
+	@GetMapping("/goToCreerParticipant")
+	public String goToCreerParticipant(Model model) {
+		List<Sport> sports = sportRepo.findAll();
+		model.addAttribute("participant", new Participant());
+		model.addAttribute("sports", sports);
+		return "creerparticipant";
+	}
+	@PostMapping("/creerParticipant")
+	public String creerParticipant( @ModelAttribute(value = "participant") Participant participant, Model model) {
+		List<Sport> sports = sportRepo.findAll();
+		model.addAttribute("sports", sports);
+		participantRepo.save(participant);
+		return "creerparticipant";
+	}
+
 }
